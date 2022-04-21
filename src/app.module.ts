ed50b48f73a952f43700a7d9712bd4e9c7a607f3
bojidaryovchev <blizzardcon10@gmail.com>
2022-04-22 01:01:44 +0300

    feat(#7): setup GraphQL and hook it to the database through Prisma

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { GraphqlModule } from './modules/graphql/graphql.module';
+import { UsersModule } from './modules/users/users.module';
 
 @Module({
-  imports: [],
+  imports: [GraphqlModule, UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
