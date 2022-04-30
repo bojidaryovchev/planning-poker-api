c5a1d658d6d186e6d7146d2a1fa24835bbf0e1d5
bojidaryovchev <blizzardcon10@gmail.com>
2022-05-01 02:57:24 +0300

    fix(#28): refresh token lifespan reduced to 1 week

--- src/modules/auth/constants/auth.constants.ts
+++ src/modules/auth/constants/auth.constants.ts
@@ -1,7 +1,7 @@
 // 15m * 60s
 export const ACCESS_TOKEN_LIFESPAN = 15 * 60;
-// 2w * 7d * 24h * 60m * 60s
-export const REFRESH_TOKEN_LIFESPAN = 2 * 7 * 24 * 60 * 60;
+// 7d * 24h * 60m * 60s
+export const REFRESH_TOKEN_LIFESPAN = 7 * 24 * 60 * 60;
 export const REFRESH_TOKEN_COOKIE_IDENTIFIER = 'refreshToken';
-// 2w * 7d * 24h * 60m * 60s * 1000ms
-export const REFRESH_TOKEN_COOKIE_LIFESPAN = 2 * 7 * 24 * 60 * 60 * 1000;
+// 7d * 24h * 60m * 60s * 1000ms
+export const REFRESH_TOKEN_COOKIE_LIFESPAN = 7 * 24 * 60 * 60 * 1000;
